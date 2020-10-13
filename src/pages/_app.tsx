a85503229c719546ff76fd129699d8f448210b49
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-10-14 03:12:39 +0500

    some changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>Multi-Step Form</title>
+        <title>Hv Multi-Step Form</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from '@material-ui/core';
 import { Form, Formik, Field, formik, FormikValues, FormikConfig } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 import React, { useState } from 'react';
-import { mixed, number, object } from 'yup';
+import { boolean, mixed, number, object, string } from 'yup';
 
 const sleep = (time) => new Promise((acc) => setTimeout(acc, time))
 
@@ -18,15 +18,23 @@ export default function Home() {
           initialValues={{
             firstName: '',
             lastName: '',
-            millionaire: false,
-            money: 0,
-            description: '',
+            email: '',
+            city: '',
+            phone: '',
+            message: '',
           }}
           onSubmit={async (values) => {
             await sleep(3000);
             console.log('values', values)
           }}>
-          <FormikStep label="Personal Info">
+          <FormikStep
+            validationSchema={object({
+              firstName: string().required('Please enter your name'),
+              lastName: string().required('Please enter your last name'),
+              email: string().email().required(),
+            })}
+
+            label="Personal Info">
             <Box paddingBottom={2}>
               <Field fullWidth name="firstName" component={TextField} label="First Name" />
             </Box>
@@ -34,38 +42,51 @@ export default function Home() {
               <Field fullWidth name="lastName" component={TextField} label="Last Name" />
             </Box>
             <Box paddingBottom={2}>
-              <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
+              <Field fullWidth name="email" component={TextField} label="Email" />
             </Box>
+            {/* <Box paddingBottom={2}>
+              <Field name="millionaire" color="primary" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
+            </Box> */}
           </FormikStep>
           <FormikStep
-            label="Bank Accounts"
+            label="More Info"
             validationSchema={object({
-              money: mixed().when('millionaire', {
-                is: true,
-                then: number()
-                  .required()
-                  .min(
-                    1_000_000,
-                    'Beacuse you said you are millionaire you need to have 1 million'
-                  ),
-                otherwise: number().required(),
-              }),
-            })}>
-            <Box paddingBottom={2}>
+              city: string().required(),
+              phone: number().typeError("That doesn't look like a phone number")
+              .positive("A phone number can't start with a minus")
+              .integer("A phone number can't include a decimal point")
+              .min(10)
+              .required('A phone number is required'),
+            } )}>
+              <Box paddingBottom={2}>
 
-              <Field
-                fullWidth
-                name="money"
-                type="number"
-                component={TextField}
-                label="All the money I have"
-              />
-            </Box>
+                <Field
+                  fullWidth
+                  name="city"
+                  type="text"
+                  component={TextField}
+                  label="City"
+                />
+              </Box>
+              <Box paddingBottom={2}>
+
+                <Field
+                  fullWidth
+                  name="phone"
+                  type="text"
+                  component={TextField}
+                  label="Mobile Number"
+                />
+              </Box>
           </FormikStep>
-          <FormikStep label="More Info">
+          <FormikStep
+            validationSchema={object({
+              message: string().required().min(15, "Field Must be have 15 or more letters")
+              
+            })}
+            label="Message">
             <Box paddingBottom={2}>
-
-              <Field fullWidth name="description" component={TextField} label="Description" />
+              <Field fullWidth name="message" component={TextField} label="Message.." />
             </Box>
           </FormikStep>
         </FormikStepper>
